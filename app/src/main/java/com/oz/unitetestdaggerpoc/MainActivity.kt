3f8bfe1312107d57d22637a258e64209d1acd46d
raul.striglio1 <raul.striglio@gmail.com>
2019-06-25 09:32:08 -0300

    inject repo in activity

--- app/src/main/java/com/oz/unitetestdaggerpoc/MainActivity.kt
+++ app/src/main/java/com/oz/unitetestdaggerpoc/MainActivity.kt
@@ -2,14 +2,26 @@ package com.res.unitetestdaggerpoc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.res.unitetestdaggerpoc.di.DaggerAppComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+
+    @Inject
+    lateinit var repo: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         DaggerAppComponent.builder().build().inject(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(this, repo.name, Toast.LENGTH_SHORT).show()
     }
 }
